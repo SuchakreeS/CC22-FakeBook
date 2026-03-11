67bd81163471e8bdd864cade972596480d24b22b
SuchakreeS <poomfail2013@gmail.com>
2026-03-11 11:48:38 +0700

    NotFound Error Middleware Added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import authRoute from './routes/auth.route.js'
+import notFoundMiddleware from './middlewares/notFound.middleware.js'
+import errorMiddleware from './middlewares/error.middleware.js'
 
 const app = express()
 
@@ -9,11 +11,17 @@ app.use('/api/auth', authRoute)
 app.use('/api/post', (req, res) => {
     res.send('post service')
 })
-app.use('/api/comment',(req, res)=>{
+app.use('/api/comment', (req, res) => {
     res.send('comment service')
 })
-app.use('/api/like',(req, res)=>{
+app.use('/api/like', (req, res) => {
     res.send('like service')
 })
+
+// Not found middleware
+app.use(notFoundMiddleware)
+
+// err middleware
+app.use(errorMiddleware)
 
 export default app
